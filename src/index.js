e76271d3195a59355c99034800726067a0b187aa
Felipe Nathan <ncampigoto@gmail.com>
2020-12-28 00:14:12 -0300

    Added and changed informations
    Changing to English
    Added Educations

--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ window.getJobs = function(){
     return {
         jobs: [{
             company: 'TOTVS S.A.',
-            period: 'Ago 2020 / Atual',
+            period: 'Aug 2020 / Current',
             projects: [{
                 tab: 'description',
-                name: "Fluig Plataforma",
-                description: "Fluig é uma plataforma que agrega vários serviços da TOTVS como BPM, ECM, LMS e outros.",
-                participations: ["Participo do projeto dando manutenção e implementando novos recursos de acordo com as demandas de clientes"],
+                name: "Fluig Platform",
+                description: "Fluig is a platform that aggregates several TOTVS services such as BPM, ECM, LMS and others",
+                participations: ["I perform maintenance and create new resources according to customer demands"],
             },{
                 tab: 'description',
                 name: "Fluig Storage",
-                description: "Um microsserviço integrado à AWS que tem por finalidade, gerenciar os arquivos importandos dentro do Fluig Plataforma.",
-                participations: ["Nele implementei o recurso de gerar um token JWT público para upload de arquivos sem a necessidade de estar logado no sistema e também a consulta de utilização do AWS S3 por meio do AWS Athena e AWS Cloudl Trail."],
+                description: "A microservice integrated with AWS that aims to manage files imported within the Fluig Platform",
+                participations: ["In it I implemented the ability to generate a public JWT token for uploading files without the need to be logged in the system and also the query for using AWS S3 through AWS Athena and AWS Cloudl Trail"],
             }],
             techs: ['Springboot', 'Hibernate', 'Java 11', 'AWS', 'Jboss', 'MySQL'],
             logo: 'assets/totvs.jpg',
@@ -27,41 +27,41 @@ window.getJobs = function(){
             projects: [{
                 tab: 'description',
                 name: "Varejonline ERP",
-                description: "Software de gestão empresarial que controla vendas, estoques, financeiro, contabilidade e fiscal, gerando as informações digitais relacionadas às demandas e obrigações do SPED",
+                description: "Business management software that controls sales, inventory, finance, accounting and tax, generating digital information related to SPED's demands and obligations",
                 participations: [
-                    'Implementação do Pagamento Escritural, troca de arquivos entre o ERP e a empresa responsável por transacionar os pagamentos bancários',
-                    'Migração de telas desenvolvidas em Flex para HTML5 e Angular 9',
+                    'Implementation of Electronic Payment, exchange of files between the ERP and the company responsible for transacting bank payments',
+                    'Migration of screens developed in Flex to HTML5 and Angular 9',
                 ],
             },{
                 tab: 'description',
                 name: "Varejonline PDV",
-                description: "Software desktop de ponto de venda, opera “On Line” ou “Off Line”, quando sem internet e como opção de contingência. Integrado ao TEF dedicado",
+                description: 'Point of sale (POS) desktop software, operates "On Line" or "Off Line", when without internet and as a contingency option. Integrated with dedicated TEF',
                 participations: [
-                    'Migração do java 7 para java 8 do PDV legado',
-                    'Implementação da integração do PDV com o Mercado Pago por meio de um microsserviço',
-                    'Implementação do envio de Cupom Fiscal Eletrônico por meio do MF-e (troca de arquivos com o aparelho integrador do estado do Ceará)',
-                    'Integração do PDV com maquineta de cartão de crédito',
+                    'Migration from java 7 to java 8 from legacy POS',
+                    'Implementation of the integration of the POS with the Mercado Pago through a microservice',
+                    'Implementation of the sending of Electronic Tax Coupon through the MF-e (exchange of files with the integrating apparatus of the state of Ceará)',
+                    'POS integration with TEF (credit card machine)',
                 ],
             },{
                 tab: 'description',
                 name: "Routed Messaging",
-                description: "Solução para mapeamento callback entre microsserviço e serviço de terceiros",
+                description: "Solution for mapping callback between microservice and third party service",
                 participations: [
-                    'Criação de um roteamento de mensagens devido ao load balancer não voltar a mensagem da integração para o servidor conectado ao PDV por meio de websocket'
+                    'Creation of a message routing due to the load balancer not returning the integration message to the server connected to the POS via websocket'
                 ],
             },{
                 tab: 'description',
                 name: "VO Mobile",
-                description: "Aplicativo mobile desenvolvido em angular com relatório de vendas por filiais e ranking de produtos e vendedores",
+                description: "Mobile application developed in angular with sales report by branches and ranking of products and sellers",
                 participations: [
-                    'Implementação de relatório de Ranking de Vendedores e Ranking de Produtos em um aplicativo feito em Ionic',
+                    'Implementation of Vendor Ranking and Product Ranking report in an application made in Ionic',
                 ]
             },{
                 tab: 'description',
                 name: "Check Windows Session",
-                description: "Aplicação web para mostrar se existe algum usuário logado no servidor compartilhado",
+                description: "Web application to show if there are any users logged on the shared server",
                 participations: [
-                    'Criação de um aplicativo desktop para validar se existe algum usuário conectado no servidor windows (usuário compartilhado)',
+                    'Creation of a web application that executes commands via cmd on the server to validate if there is any user connected to the windows server (shared user) with Kotlin and Thymeleaf',
                 ],
             }],
             techs: ['Springboot', 'Hibernate', 'Java 8', 'PostreSQL', 'Oracle', 'Tomcat', 'Angular 9', 'jQuery', 'Java SWT', 'ActiveMQ', 'Ionic', 'Typescript', 'Spring Stomp', 'Kotlin'],
@@ -73,11 +73,11 @@ window.getJobs = function(){
             projects: [{
                 tab: 'description',
                 name: "Fusion Platform",
-                description: "Fusion para gerenciamento de documentos e gerenciamento de processos criado pela Neomind",
+                description: "Fusion for document management and process management created by Neomind",
                 participations: [
-                    'Criação do relatório dinâmico, transformando o formulário dinâmico do Fusion em uma estrutura para impressão no ireport',
-                    'Criação e manutenção de workflows de clientes',
-                    'Criação da nova funcionalidade de logtime, listando dos os projetos no nome do usuário para informar as horas trabalhadas',
+                    'Dynamic report creation, transforming the Fusion dynamic form into a structure for printing on ireport',
+                    'Creation and maintenance of customer workflows',
+                    `Creation of the new logtime functionality, listing all projects in the user's name to inform the hours worked`,
                 ],
             }],
             techs: ['Java EE', 'Hibernate', 'Java 7', 'Tomcat', 'SQL Server', 'AngularJS', 'jQuery'],
@@ -89,12 +89,12 @@ window.getJobs = function(){
             projects: [{
                 tab: 'description',
                 name: "Microvix ERP",
-                description: "Microvix ERP é um sistema para gestão empreserial que atende todas as rotinas das redes e franquias, apoiando o relacionamento franqueador-franqueado e oferecendo soluções eficientes para os demais processos financeiros",
+                description: "Microvix ERP is a system for business management that meets all the routines of networks and franchises, supporting the franchisor-franchisee relationship and offering efficient solutions for other financial processes",
                 participations: [
-                    'Manutenção do ERP, correção de bugs e regras de negócio (ASP)',
-                    'Implementação do ASP-ORM, mapeando as tabelas em classes de ASP',
-                    'Migração de páginas com programação funcional para páginas com orientação a objetos e padrão MVC (ASP, JQuery)',
-                    'Criação da rotina de venda de óculos e/ou armações, validando regras de agregação dos produtos (C#)',
+                    'Maintenance of ERP, bug fixes and business rules with ASP language',
+                    'Implementation of ASP-ORM, mapping database tables into ASP objects respecting the concepts of object-oriented programming',
+                    'Migration of pages with functional programming to pages with object orientation and MVC standard (ASP, JQuery)',
+                    'Creation of the glasses and / or frames sales routine, validating product aggregation rules (C#)',
                 ],
             }],
             techs: ['ASP3', 'C#', 'SQL Server', 'IIS', 'jQuery'],
@@ -106,9 +106,9 @@ window.getJobs = function(){
             projects: [{
                 tab: 'description',
                 name: "GECEX",
-                description: "Sistema para gestão de comércio exterior",
+                description: "Foreign trade management system",
                 participations: [
-                    'Estágio em progração com Power Builder, os analistas de sistemas passavam poucas demandas a serem executadas, sendo o mais comum, troca de valor de labels',
+                    'Internship in programming with Powerbuilder, systems analysts spent few demands to be executed, the most common being the exchange of labels',
                 ],
             }],
             techs: ['Power Builder'],
@@ -123,12 +123,12 @@ window.reverse = (order) => order % 2 == 0
 window.getInfo = function() {
     return {
         name: "Felipe Nathan Campigoto",
-        age: `${getAge()} anos`,
+        age: `${getAge()} years old`,
         location: "Joinville - Santa Catarina - Brasil",
         resume: `
-            Sou um desenvolvedor que gosta de ler sobre a linguagem que utilizo para trabalhar e também sobre novas linguagens que tenho vontade de apreder.
-            Nas horas vagas gosto de estudar sobre alguma linguagem que tenho curiosidade, como, atualmente além do java, estou estudando sobre ruby.
-            Além de estudar, gosto de me descontrair com jogos de computador, assistindo séries com minha esposa ou brincar com nossos pets.
+            <p class="text-center"> In a few words, I'm a software developer that accepts challenges, I already worked with desktop, web and mobile applications. </p>
+            <p class="text-center"> One of my favorites hobbies is learn about programming languages drinking some tasty beer, sure, if don't have a drop of coffee ;) </p>
+            <p class="mt-2 text-center"> And to rest, I like to get stressed with online games or watch some series with my family </p>
         `.trim()
     }
 }
@@ -164,6 +164,17 @@ window.getSkills = function() {
             level: 20,
             tools: ['Bundle', 'Rails', 'Puma']
         }].sort((s1, s2) => s1.level >= s2.level ? -1 : 1)
+    }
+}
+
+window.getEducations = function() {
+    return {
+        items: [{
+            name: 'Universidade Sociedade Educacional de Santa Catarina',
+            period: '2011 / 2015',
+            course: `Bacharelor's Degree, Information System`,
+            local: 'Joinville, Brazil'
+        }]
     }
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: [],
   darkMode: 'media', // false or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'montserrat': ['Montserrat', 'ui-serif', 'Georgia, Cambria', 'Times New Roman', 'Times', 'serif']
+    },
     extend: {},
   },
   variants: {
